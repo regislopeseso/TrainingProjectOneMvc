9ca763df9c4b700e300aec2333e7418da6d39a4c
Regis <regis.lopes.eso@gmail.com>
2025-01-24 10:50:03 -0300

    Atualização 20250124

--- ControleEmpresasFuncionariosMvc/wwwroot/js/site.js
+++ ControleEmpresasFuncionariosMvc/wwwroot/js/site.js
@@ -2,3 +2,20 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+function voltarPagina() {
+    if (window.history.length > 1) {
+        history.back();
+    } else {
+        alert("Não há uma página anterior no histórico!");
+    }
+}
+
+// Ocultar o botão se não houver histórico
+window.onload = function () {
+    const botaoVoltar = document.getElementById('botao-voltar');
+    if (window.history.length <= 1) {
+        botaoVoltar.style.display = 'none';
+    }
+};
